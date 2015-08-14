6b14be6b4d7771db115bfe9ebcbe9deb4b6adbcd
Allison Lara <allison@simple.com>
2015-08-13 19:55:19 -0700

    Cute up the git info in prompt and say hi

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,23 +1,24 @@
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_hide_untrackedfiles 1
 
-set -g __fish_git_prompt_color_branch magenta -b
-set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_color_branch magenta
+
+set -g __fish_git_prompt_show_upstream "informative"
 set -g __fish_git_prompt_char_upstream_ahead "↑"
 set -g __fish_git_prompt_char_upstream_behind "↓"
 set -g __fish_git_prompt_char_upstream_prefix ""
 
-set -g __fish_git_prompt_char_stagedstate "●"
-set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_stagedstate "⊕"
+set -g __fish_git_prompt_char_dirtystate "Δ"
 set -g __fish_git_prompt_char_untrackedfiles "."
-set -g __fish_git_prompt_char_conflictedstate "✖"
-set -g __fish_git_prompt_char_cleanstate "✔"
+set -g __fish_git_prompt_char_conflictedstate "⍉"
+# -set -g __fish_git_prompt_char_cleanstate "✔"
 
-set -g __fish_git_prompt_color_dirtystate blue
-set -g __fish_git_prompt_color_stagedstate yellow
-set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_dirtystate $fish_color_command
+set -g __fish_git_prompt_color_stagedstate $fish_color_quote
+set -g __fish_git_prompt_color_invalidstate $fish_color_error
 set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -g __fish_git_prompt_color_cleanstate green -b
+set -g __fish_git_prompt_color_cleanstate $fish_color_redirection
 
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
@@ -34,7 +35,7 @@ function fish_prompt --description 'Write out the prompt'
 	printf '%s \n' (__fish_git_prompt)
 
 	if not test $last_status -eq 0
-	set_color $fish_color_error
+        set_color $fish_color_error
 	end
 
 	echo -n 'λ '
