a4ad4fab9f8d0a0d85de1c7f695e1199b959012c
Allison Lara <allison@simple.com>
2015-08-24 14:09:45 -0700

    It's vim!

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-ln -fs rc/tmux.conf ~/.tmux.conf
-ln -fs rc/spacemacs ~/.spacemacs
+pushd ~
+ln -fs rc/tmux.conf .tmux.conf
+ln -fs rc/spacemacs .spacemacs
+ln -fs rc/vim/vimrc .vimrc
+ln -fs rc/vim .vim 
+popd
 
 mkdir -p ~/.config
 pushd ~/.config
---
+++ vim/bootstrap.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+set -x
+
+git clone https://github.com/gmarik/Vundle.vim bundle/Vundle.vim
+vim +BundleInstall +qall
+
+cd bundle/YouCompleteMe
+./install.sh --clang-compiler
+
