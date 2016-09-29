398ab3fdb6bdcf97eb9a79f42d9d71db220c66be
Allison Lara <allison@alliesaur.us>
2016-09-29 11:20:22 -0700

    Make shiftmanager alias use prod creds

--- fish/functions/shiftmanager.fish
+++ fish/functions/shiftmanager.fish
@@ -1,6 +1,6 @@
 function shiftmanager
 	cd ~/src/analytics/shiftmanager-simple
     set -x PGDATABASE analytics
-    envcrypt ~/.secrets/redshift.gpg $PWD/venv/bin/ipython -im shiftmanager_simple
+    envcrypt ~/.secrets/redshift-prod.gpg $PWD/venv/bin/ipython -im shiftmanager_simple
     cd -
 end
