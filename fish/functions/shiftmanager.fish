eb9270e8b2be0d79f341cb4923ecb865d1e5fb8c
Allison Lara <allison@alliesaur.us>
2018-09-13 15:42:04 -0700

    Cleanup things before bailing on Simple

--- fish/functions/shiftmanager.fish
+++
@@ -1,6 +0,0 @@
-function shiftmanager
-	cd ~/src/analytics/shiftmanager-simple
-    set -x PGDATABASE analytics
-    envcrypt ~/.secrets/redshift-prod.gpg $PWD/venv/bin/ipython -im shiftmanager_simple
-    cd -
-end
